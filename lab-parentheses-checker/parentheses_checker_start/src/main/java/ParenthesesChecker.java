c460a6a4462822b897d7a7b5bcb661ee23c66213
bendesousa <bdsousa069@gmail.com>
2022-08-26 10:04:38 +0100

    attempted MVP

---
+++ lab-parentheses-checker/parentheses_checker_start/src/main/java/ParenthesesChecker.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class ParenthesesChecker {
+
+    public static boolean checkParentheses(String testString) {
+        if ((testString.length() % 2) == 1) return false;
+        else {
+            Stack<Character> s = new Stack<>();
+            for (char bracket : testString.toCharArray())
+                switch (bracket) {
+                    case '{': s.push('}'); break;
+                    case '(': s.push(')'); break;
+                    case '[': s.push(']'); break;
+                    case '<': s.push('>'); break;
+                    default :
+                        if (s.isEmpty() || bracket != s.peek()) { return false;}
+                        s.pop();
+                }
+            return s.isEmpty();
+        }
+    }
+
+//    private boolean checker;
+//    private String testString;
+//
+//    public boolean checkParentheses (String testString){
+//        if (null == testString || ((testString.length() % 2) != 0)) {
+//            return false;
+//        } else {
+//            char[] ch = testString.toCharArray();
+//            for (char c : ch) {
+//                if (!(c == '{' || c == '[' || c == '(' || c == '<' || c == '}' || c == ']' || c == ')' || c == '>' )) {
+//                    return false;
+//                } else{
+//                    return true;}
+//            }
+//        } return true;
+//    }
+
+
+}
